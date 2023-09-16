ef172c854d0d74da64eddda6ff99119940930db0
Hansen Wu <hansen.wu@pinhome.id>
2023-09-16 17:24:09 +0700

    fix

---
+++ resources/js/pages/invitation/Invitation.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="home">Hi, {{ user.name }}</div>
+</template>
+
+<style>
+    .home {
+        font-family: Brigitha Signature;
+    }
+</style>
+
+<script>
+    import requestUrl from "../../helper/request"
+
+    export default {
+        data() {
+            return {
+                user: {}
+            }
+        },
+        mounted() {
+            this.getInvitationDetail() 
+        },
+        methods: {
+            getInvitationDetail() {
+                requestUrl.get("/invitations/" + this.getTokenFromURL()).then((response) => {
+                    if (response.data == null && response.data.data == null) {
+                        this.redirectToNotFound()
+                        return
+                    }
+
+                    this.user = response.data.data
+                })
+
+            },
+            getTokenFromURL() {
+                return this.$route.params.token
+            },
+            redirectToNotFound() {
+                this.$router.push({
+                    name: "Not Found"
+                })
+            },
+        }
+    }
+</script>
