4fe379865743333e39790e9c4788f13d2b3329ee
Hansen Wu <hansen.wu@pinhome.id>
2023-09-24 18:15:56 +0700

    fix

--- resources/js/pages/invitation/child/rsvp.vue
+++ resources/js/pages/invitation/child/rsvp.vue
@@ -10,28 +10,29 @@
                 </div>
                 <div class="rsvp-form-control">
                     <div class="rsvp-form-select">Attendance</div>
-                    <select class="rsvp-form-select-value">
-                        <option value="">Yes</option>
-                        <option value="">No</option>
+                    <select class="rsvp-form-select-value" v-model="form.is_attend">
+                        <option value="yes">Yes</option>
+                        <option value="no">No</option>
                     </select>
                 </div>
-                <div class="rsvp-form-control">
+                <div class="rsvp-form-control" v-if="form.is_attend == 'yes'">
                     <div class="rsvp-form-input">Email</div>
-                    <input class="rsvp-form-input-value" type="text">
+                    <input class="rsvp-form-input-value" type="text" placeholder="Input email" v-model="form.email" @keyup="validateEmail()">
+                    <div v-show="error.email != ''" class="rsvp-form-error-message">{{ error.email }}</div>
                 </div>
-                <div class="rsvp-form-control">
+                <div class="rsvp-form-control" v-if="form.is_attend == 'yes'">
                     <div class="rsvp-form-select">Number of Pax</div>
-                    <select class="rsvp-form-select-value">
-                        <option value="">1</option>
-                        <option value="">2</option>
+                    <select class="rsvp-form-select-value" v-model="form.number_of_pax">
+                        <option :value="i" v-for="i in user.max_attendance">{{ i }}</option>
                     </select>
                 </div>
                 <div class="rsvp-form-control">
                     <div class="rsvp-form-textarea">Wishes for Ryan & Winnie</div>
-                    <textarea class="rsvp-form-textarea-value" rows="4"></textarea>
+                    <textarea class="rsvp-form-textarea-value" rows="4" v-model="form.wishes" placeholder="Input wishes" @keyup="validateWishes()"></textarea>
+                    <div v-show="error.wishes != ''" class="rsvp-form-error-message">{{ error.wishes }}</div>
                 </div>
 
-                <div class="rsvp-confirmation-button">Confirmation</div>
+                <div class="rsvp-confirmation-button" @click="confirmation()">Confirmation</div>
             </div>
         </div>
         
@@ -81,6 +82,10 @@
         color: #5E5E5E;
     }
 
+    .rsvp-form-textarea-value {
+        vertical-align: top;
+    }
+
     .rsvp-form-disable {
         background-color: #dbdbdb;
     }
@@ -95,10 +100,84 @@
         margin: 25px auto;
         cursor: pointer;
     }
+
+    .rsvp-form-error-message {
+        margin-top: 6px;
+        color: red;
+        font-size: 14px;
+    }
 </style>
 
 <script>
     export default {
-        props: ['user']
+        props: ['user'],
+        data() {
+            return {
+                form: {
+                    is_attend: "yes",
+                    email: "",
+                    number_of_pax: 1,
+                    wishes: ""
+                },
+                error: {
+                    email: "",
+                    wishes: ""
+                },
+                isSubmitted: false
+            }
+        },
+        methods: {
+            confirmation() {
+                this.isSubmitted = true
+                const isSuccess = this.validate()
+                if (isSuccess) {
+                    this.submitRSVP()
+                }
+            },
+            validate() {
+                const isEmailValid = this.validateEmail()
+                const isWishesValid = this.validateWishes()
+
+                return isEmailValid && isWishesValid
+            },
+            validateEmail() {
+                if (!this.isSubmitted) {
+                    return false
+                }
+
+                if (this.form.email == "") {
+                    this.error.email = "Email is required"
+                    return false
+                }
+
+                if (!this.isValidEmail(this.form.email)) {
+                    this.error.email = "Email format is invalid"
+                    return false
+                }
+
+                this.error.email = ""
+                return true
+            },
+            validateWishes() {
+                if (!this.isSubmitted) {
+                    return false
+                }
+                
+                if (this.form.wishes == "") {
+                    this.error.wishes = "Wishes is required"
+                    return false
+                }
+
+                this.error.wishes = ""
+                return true
+            },
+            isValidEmail(email) {
+                const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+                return emailRegex.test(email);
+            },
+            submitRSVP() {
+
+            }
+        }
     }
 </script>
