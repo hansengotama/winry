c5c1a463e4ff71ce68298daddc1ac747196d76d4
Hansen Wu <hansen.wu@pinhome.id>
2023-09-28 17:31:52 +0700

    fix

--- resources/js/pages/invitation/Invitation.vue
+++ resources/js/pages/invitation/Invitation.vue
@@ -99,7 +99,9 @@
                 this.isLoading = false
             }
 
-            window.addEventListener('load', this.isLoading = false)
+            window.addEventListener('load', () => {
+                this.isLoading = false
+            })
         },
         components: {
             Opening: Opening,
